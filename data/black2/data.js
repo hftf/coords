3a0149492cccfe368a837d7a2f753c9927c232c3
Ophir Lifshitz <hangfromthefloor@gmail.com>
2014-07-01 16:53:01 -0400

    Add ref key

--- data/black2/data.js
+++ data/black2/data.js
@@ -1,5 +1,12 @@
 var game = 'black2';
 var bounds =                [  0,   0,   255, 191];
+var ref_key = {
+	"\\w{3}":	"ID of button",
+	"END":		"End of flow",
+	"NEW":		"Unimplemented (TODO)",
+	"   ":		"Unknown (FIXME)",
+};
+
 var coords = {
 "Battle": {
 	"id": "b",
