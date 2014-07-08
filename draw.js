hftf/coords

commit 8950a889e2b7b449f825b2e3ae3a9117c6e67676

Link to contribute missing image

draw.js
@@ -53,7 +53,13 @@ var Draw = (function() {
 			var layers = document.createElement('div');
 			layers.setAttribute('class', 'layers');
 			layers.appendChild(c);
-			layers.insertAdjacentHTML('afterbegin', '<img src="data/' + game + '/screens/' + parent + '/' + menu + '.png">');
+
+			var img = document.createElement('img'),
+				src = 'data/' + game + '/screens/' + parent + '/' + menu + '.png';
+			img.onerror = this.error;
+			img.setAttribute('src', src);
+			layers.appendChild(img);
+
 			div.appendChild(layers);
 			section.appendChild(div);
 
@@ -102,6 +108,13 @@ var Draw = (function() {
 			parent_ctx.fillRect.apply(parent_ctx, xywh);
 
 			return div;
+		},
+		error: function(e) {
+			var div = this.parentNode.parentNode;
+			div.insertAdjacentHTML('beforeend',
+				'<p class="contrib-image"><a href="https://github.com/hftf/coords/issues/42">Contribute this screen</a></p>'
+			);
+			console.log(this, e);
 		},
 	};
 

coords.css
@@ -84,6 +84,10 @@ section {
 .menu-heading .layers img { opacity: 0; }
 .menu-heading .layers:hover img { opacity: 1; }
 
+.contrib-image {
+	font-size: 0.8em;
+}
+
 .category h2 {
 	margin-top: 2em;
 	border-left: 6px solid #bbb;

commit 114181602792fb913d0101cea8f77184b7e77251

Move screenshots behind canvas (broken by 8950a88)

draw.js
@@ -52,13 +52,14 @@ var Draw = (function() {
 
 			var layers = document.createElement('div');
 			layers.setAttribute('class', 'layers');
-			layers.appendChild(c);
 
 			var img = document.createElement('img'),
 				src = 'data/' + game + '/screens/' + parent + '/' + menu + '.png';
 			img.onerror = this.error.bind(div);
 			img.setAttribute('src', src);
 			layers.appendChild(img);
+
+			layers.appendChild(c);
 
 			div.appendChild(layers);
 			section.appendChild(div);