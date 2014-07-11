hftf/coords

commit ebb396fb1300b94f01849c5706e7dac926aa4b1c

Simplified URL regex and realigned diagram

state.js
@@ -47,10 +47,10 @@ var State = (function() {
 			if (search === '')
 				window.location.search = '?' + this.state.game;
 
-			// '?hg;x,y+a,b,c-d,e,f'
-			//                      ? hg           + a,b,c           - d,e,f         ; x,y
-			//                      - --------     - ---------       - --------      - -------
-			var m = search.match(/^\?(\w+)(?:\+([\wé,. ]+))?(?:-([\wé,. ]+))?(?:;(\d+,\d+))?$/);
+			// '?hg+a,b-c,d;x,y'
+			//                      ? hg      + a,b      - c,d      ; x,y
+			//                      ^ 1--     ^ 2--      ^ 3--      ^ 4------
+			var m = search.match(/^\?(\w+)(?:\+(\w+))?(?:-(\w+))?(?:;(\d+,\d+))?$/);
 
 			if (!m)
 				throw 'Invalid query string: ' + search;

commit bbbb9fae66a3cdebaf65203ce2f9a83b2c43d1f8

Forgot about commas (broken in ebb396f)

state.js
@@ -48,9 +48,9 @@ var State = (function() {
 				window.location.search = '?' + this.state.game;
 
 			// '?hg+a,b-c,d;x,y'
-			//                      ? hg      + a,b      - c,d      ; x,y
-			//                      ^ 1--     ^ 2--      ^ 3--      ^ 4------
-			var m = search.match(/^\?(\w+)(?:\+(\w+))?(?:-(\w+))?(?:;(\d+,\d+))?$/);
+			//                      ? hg      + a,b         - c,d         ; x,y
+			//                      ^ 1--     ^ 2-----      ^ 3-----      ^ 4------
+			var m = search.match(/^\?(\w+)(?:\+([\w,]+))?(?:-([\w,]+))?(?:;(\d+,\d+))?$/);
 
 			if (!m)
 				throw 'Invalid query string: ' + search;