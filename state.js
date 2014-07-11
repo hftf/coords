9a18c285611cc12ce489fa16d711c6760b338e28
Ophir Lifshitz <hangfromthefloor@gmail.com>
2014-07-10 21:55:41 -0400

    Move ready handlers to onset.js

--- state.js
+++ state.js
@@ -168,32 +168,3 @@ var State = (function() {
 
 	return State;
 })();
-
-document.addEventListener('DOMContentLoaded', function() {
-	Load.all();
-
-	var inputs = document.querySelectorAll('.menu-list input');
-	for (var i in inputs) {
-		inputs[i].onclick = function() {
-			State.rotateState(this);
-		};
-	}
-	inputs = document.querySelectorAll('.menu-heading input');
-	for (var i in inputs) {
-		inputs[i].onclick = function() {
-			State.rotateStates(this);
-		};
-	}
-
-	draw();
-	try {
-		State.setAllFromUrl();
-	}
-	catch (e) {
-		// TODO display error to user?
-		console.error(e);
-	}
-});
-window.onload = function() {
-	Load.grid();
-};
