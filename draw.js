9f558f00f6291f7795e3982bf3e80a09891dd182
Ophir Lifshitz <hangfromthefloor@gmail.com>
2014-07-12 02:24:14 -0400

    Show button refs

--- draw.js
+++ draw.js
@@ -105,18 +105,56 @@ var Draw = (function() {
 			if (current_color >= palette.length)
 				current_color = 0;
 
+			var rekt_obj = coords[category][menu][rekt];
+
 			div.insertAdjacentHTML('beforeend',
 				'<label for="' + checkbox_id + '">' +
 				'<strong>' + rekt + '</strong>' +
-				'<small style="background: ' + color + ';"><span>' + format_x1y1.apply(null, coords[category][menu][rekt].coords) + '</span></small>' +
+				'<small style="background: ' + color + ';"><span>' + format_x1y1.apply(null, rekt_obj.coords) + '</span></small>' +
 				'</label>');
 
+			div.appendChild(this.refs(this._joinIds(grandparent, parent), rekt_obj.ref));
+
 			parent_ctx.fillStyle = color;
-			var xywh = to_xywh(coords[category][menu][rekt].coords);
+			var xywh = to_xywh(rekt_obj.coords);
 			parent_ctx.fillRect.apply(parent_ctx, xywh);
 
 			return div;
 		},
+		refs: (function() {
+			function index_f(i) { return '➀➁➂➃'[i]; }
+			function fixme_filter(to) { return to !== '   '; }
+			function to2link(to, i, a) {
+				var title, link_text, link;
+				var index = (a.length > 1) ? index_f(i) + ' ' : '';
+
+				if (to in ref_key) {
+					title = ref_key[to];
+					link_text = to;
+					link = index + '<span title="' + title + '" class="' + to + '">' + link_text + '</span>';
+				}
+				else {
+					var path = Load.lookup[to];
+					title = path.join(' → ');
+					link_text = path[path.length - 1];
+					link = index + '<a title="' + title + '" href="#' + to + '">' + link_text + '</a>';
+				}
+
+				return link;
+			}
+
+			return function(from, tos) {
+				var div = document.createElement('div');
+				div.setAttribute('class', 'refs');
+
+				if ('string' === typeof tos)
+					tos = [tos];
+
+				div.innerHTML = tos.filter(fixme_filter).map(to2link).join(', ');
+
+				return div;
+			};
+		})(),
 		error: function(e) {
 			this.insertAdjacentHTML('beforeend',
 				'<p class="contrib-image"><a href="https://github.com/hftf/coords/issues/42">Contribute this screen</a></p>'
